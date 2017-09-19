a6d8a35948d554e66412c5ae4bba65fc30e7670f
Andreas2200 <Andreas.c.gade@gmail.com>
2017-09-19 14:55:17 +0200

    Startet to test some things and eventually deleted them again

---
+++ HackTheSchool/HackTheSchool/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    int Speed = 5;
+
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        /*if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {*/
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * Speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * Speed);
+        //}
+	}
+}
